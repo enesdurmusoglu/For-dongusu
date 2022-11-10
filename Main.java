ae8121e874e9b92fe23e75fc9ec81741d94cd792
Enes Durmuşoğlu <73403774+enesdurmusoglu@users.noreply.github.com>
2022-11-10 21:43:34 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // küçük bir for döngüsü örneği 
+
+        for (int i=1; i<=100; i++){
+            System.out.println(i);
+        }
+        System.out.println("Döngü Bitti");
+
+
+
+    }
+}
